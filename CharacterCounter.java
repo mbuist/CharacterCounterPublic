4523792184365b7d367f836f1ed36ef9bcafb181
mbuist <mbuist25@gmail.com>
2024-07-30 23:52:08 -0400

    Create CharacterCounter.java public version

---
+++ CharacterCounter.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author Mary Buist
+ * @author Anna Lu Write the CharacterCounter program inside the main method
+ * according to the assignment description.
+ *
+ * To compile: javac CharacterCounter.java To execute: java CharacterCounter
+ * input1.in output.out java CharacterCounter "pride&prejudice.in" output.out
+ *
+ * DO NOT change the class name DO NOT use System.exit() DO NOT change add
+ * import statements DO NOT add project statement DO NOT add helper methods
+ *
+ */
+public class CharacterCounter {
+
+    public static void main(String[] args) {
+        // YOUR CODE HERE //
+
+        /**
+         * Students will implement two parallel arrays and ASCII values
+         *  to monitor frequency of characters in txt input file.
+         *  
+         * They will print out a report for all the alphabetic and standard punctuation characters
+         */
+    }
+}
